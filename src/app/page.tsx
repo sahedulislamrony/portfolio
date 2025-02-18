a1faa1a0e61095797254bea931fcaf6be6c082b5
Sahedul Islam Rony <sahedul.dev@gmail.com>
2025-02-18 20:45:34 +0600

    Comoleted Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
+import Navbar from "./sections/Navbar";
+
 export default function Home() {
   return (
-    <div className="text-red-500 w-screen h-screen text-7xl flex justify-center items-center font-extrabold">
-      Welcome Home.
-    </div>
+    <>
+      <Navbar />
+      <div
+        id="home"
+        className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold"
+      >
+        Welcome Home.
+      </div>
+      <div className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold">
+        Welcome Home.
+      </div>
+      <div
+        id="contact"
+        className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold"
+      >
+        Welcome Home.
+      </div>
+      <div className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold">
+        Welcome Home.
+      </div>
+      <div
+        id="about"
+        className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold"
+      >
+        Welcome Home.
+      </div>
+      <div className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold">
+        Welcome Home.
+      </div>
+      <div className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold">
+        Welcome Home.
+      </div>
+      <div className="   bg-no-repeat bg-cover bg-center w-full text-highlight h-screen text-7xl flex justify-center items-center font-extrabold">
+        Welcome Home.
+      </div>
+    </>
   );
 }
---
+++ src/app/sections/Navbar.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { SparklesText } from "@/components/magicui/sparkles-text";
+import { useTheme } from "next-themes";
+import { Download, Moon, Rocket, Sparkles, Sun } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 bg-white/5 dark:bg-black/5 backdrop-blur-lg z-50">
+        {/* main nav  */}
+        <div className="flex justify-between items-center p-4 bg-transparent relative spaceX">
+          {/* Left - Logo */}
+          <LeftSec />
+
+          {/* Center - Menu */}
+          <CenterSec />
+
+          {/* Right - Buttons & Theme Toggle */}
+          <RightSec />
+        </div>
+
+        {/* Gradient Line */}
+        <GradientLine />
+      </div>
+    </>
+  );
+}
+
+function LeftSec() {
+  return (
+    <div className="flex items-center  flex-1">
+      <SparklesText
+        text="Sahedul."
+        className="text-3xl font-bold text-white cursor-pointer select-none"
+        sparklesCount={5}
+        colors={["#0ea5e9", "#fff", "#0ea5e9", "#00ccff"]}
+      />
+    </div>
+  );
+}
+
+function CenterSec() {
+  return (
+    <div
+      className={cn(
+        // base design
+        "flex space-x-10 text-gray-300 text-lg font-bold  flex-2 items-center justify-center"
+        // responsive design
+      )}
+    >
+      <NavItem href="#home" text="Home" />
+      <NavItem href="#about" text="About" />
+      <NavItem href="#skills" text="Skills" />
+      <NavItem href="#projects" text="Projects" />
+      <NavItem href="#contact" text="Contact" />
+    </div>
+  );
+}
+
+function NavItem({
+  href,
+  text,
+  className,
+}: {
+  href: string;
+  text: string;
+  className?: string;
+}) {
+  return (
+    <a href={href} className={cn("hover:text-sky-400", className)}>
+      {text}
+    </a>
+  );
+}
+
+function RightSec() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div className="flex items-center justify-end space-x-4  flex-1">
+      <Button variant="outline" className=" text-gray-300 hover:text-sky-400">
+        <Download /> Download CV
+      </Button>
+      <Button className="bg-gradient-to-r from-sky-700 to-indigo-500 text-gray-100">
+        <Rocket /> Hire Me
+      </Button>
+
+      <button
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="p-2 rounded-full bg-white/10 hover:bg-white/20 transition"
+      >
+        {theme === "dark" ? <Sun size={20} /> : <Moon size={20} />}
+      </button>
+    </div>
+  );
+}
+
+function GradientLine() {
+  return (
+    <div className="w-full relative mx-auto">
+      <div className="absolute left-1/2 top-0 -translate-x-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+      <div className="absolute left-1/2 top-0 -translate-x-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+      <div className="absolute left-1/2 top-0 -translate-x-1/2 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[10px] w-1/4 blur-sm" />
+      <div className="absolute left-1/2 top-0 -translate-x-1/2 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[2px] w-1/4" />
+    </div>
+  );
+}
