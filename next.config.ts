7cda041b451d7291f7c79948df6dcec2625c41b0
Sahedul Islam Rony <sahedul.dev@gmail.com>
2025-03-11 16:10:38 +0600

    Updated routing in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: [
-  //     "api.microlink.io",
-  //   ],
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/anonymous",
+        destination: "https://getmsg.netlify.app/sahed",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
